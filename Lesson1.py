a1814f10de4797bd8cf34bbd87ab42694108925a
Vasiliy Sadovnikov <basilsad1989@gmail.com>
2020-03-09 15:21:01 +0200

    initial commit lesson1

---
+++ Lesson1.py
@@ -0,0 +1,16 @@
+#Operations
+a=2
+b=1
+c=a-b
+e = a + b
+d = a / b
+f= a * b
+print(e, c, d, f)
+
+#variables
+average = 17/2
+print(average)
+
+#assin variabes and use
+name = 'Vasyl'
+print("My name is " + name)
